9a025d16738be11b2fdeb73a81b9d55fae2de8c2
gaeng02 <sondin144@gmail.com>
2024-10-28 20:31:18 +0900

    api_module

---
+++ src/api_module.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path = "../.env")
+
+headers = {
+    "x-nxopen-api-key": os.getenv("API_KEY")
+}
+
+# urlString = "https://open.api.nexon.com/maplestory/v1/id?" + characterName
+# urlString = "https://open.api.nexon.com/maplestory/v1/character/list?" + characterName
+# urlString = "https://open.api.nexon.com/maplestory/v1/character/basic?ocid=" + ocid
+# urlString = "https://open.api.nexon.com/maplestory/v1/character/item-equipment?ocid=" + ocid
+
+'''
+response = requests.get(urlString, headers = headers)
+
+with open("out.json", "w", encoding='utf-8') as f :
+    json.dump(response.json(), f, ensure_ascii = False, indent = 4)
+'''
+
+print("Done")
